67ee08294f969e90d6052757cbae30fa3c981c40
samarth-2 <samarth.2032@gmail.com>
2025-10-14 20:20:52 +0530

    client side setup

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='min-h-screen w-full bg-gray-500'>
+      
+    </div>
+  )
+}
+
+export default App
